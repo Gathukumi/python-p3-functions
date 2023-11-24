7fc9e10020f6079e2b1f5d6ae1af7fab45a0bb99
Gathukumi <gathukumidavid001@gmail.com>
2023-11-24 16:44:04 +0300

    completed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(name = "Hello, programmer!"):
+    print(f"{name}")
 
-def greet(name):
-    pass
+def greet(name="david"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Denis");
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+result = add(5, 7)
+print(result)
 
 def halve(number):
-    pass
+    return number / 2
+
+result = halve(20)
+print(result)
